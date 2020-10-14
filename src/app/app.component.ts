6dc96f36e2a6e206549a7cc81ddbc46b30c72e22
cbevel <chelsey.bevel@gmail.com>
2020-10-13 22:34:16 -0400

    added 's' to title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Your Thought Space';
+  title = 'Your Thoughts Space';
 }
