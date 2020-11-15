c3dee40035b136489cdb675cdbaae9ae8544e122
cbevel <chelsey.bevel@gmail.com>
2020-11-15 13:17:22 -0500

    added new login-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule
